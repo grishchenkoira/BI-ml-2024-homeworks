c089204387afc7e36c82f34624b1df0873c410b9
Irina Grishchenko <grishch.irina@gmail.com>
2024-02-11 06:54:34 +0300

    Add regression metrics

--- HW1/metrics.py
+++ HW1/metrics.py
@@ -15,11 +15,16 @@ def binary_classification_metrics(y_pred, y_true):
     # Some helpful links:
     # https://en.wikipedia.org/wiki/Precision_and_recall
     # https://en.wikipedia.org/wiki/F1_score
-
-    """
-    YOUR CODE IS HERE
-    """
-    pass
+    true_positive = np.sum((y_true == 1) & (y_pred == 1))
+    false_positive = np.sum((y_true == 0) & (y_pred == 1))
+    false_negative = np.sum((y_true == 1) & (y_pred == 0))
+    true_negative = np.sum((y_true == 0) & (y_pred == 0))
+    
+    precision_score = true_positive / (true_positive + false_positive + 1e-9)
+    recall_score = true_positive / (true_positive + false_negative + 1e-9)
+    f1_score = 2 * (precision_score * recall_score) / (precision_score + recall_score + 1e-9)
+    accuracy_score = (true_positive + true_negative) / (true_positive + true_negative + false_positive + false_negative)
+    return precision_score, recall_score, f1_score, accuracy_score
 
 
 def multiclass_accuracy(y_pred, y_true):
@@ -35,7 +40,20 @@ def multiclass_accuracy(y_pred, y_true):
     """
     YOUR CODE IS HERE
     """
-    pass
+    unique_classes = np.unique(np.concatenate((y_true, y_pred)))
+    num_classes = len(unique_classes)
+    confusion_matrix = np.zeros((num_classes, num_classes))
+    for i in range(len(y_true)):
+        true_class_idx = np.where(unique_classes == y_true[i])[0][0]
+        pred_class_idx = np.where(unique_classes == y_pred[i])[0][0]
+        confusion_matrix[true_class_idx, pred_class_idx] += 1
+    
+    precision_score = np.sum(np.diag(confusion_matrix) / (np.sum(confusion_matrix, axis=0) + 1e-9)) / num_classes
+    recall_score = np.sum(np.diag(confusion_matrix) / (np.sum(confusion_matrix, axis=1) + 1e-9)) / num_classes
+    f1_score = 2 * (precision_score * recall_score) / (precision_score + recall_score + 1e-9)
+    accuracy_score = np.trace(confusion_matrix) / np.sum(confusion_matrix)
+
+    return precision_score, recall_score, f1_score, accuracy_score
 
 
 def r_squared(y_pred, y_true):
@@ -48,10 +66,11 @@ def r_squared(y_pred, y_true):
     r2 - r-squared value
     """
 
-    """
-    YOUR CODE IS HERE
-    """
-    pass
+    y_mean = sum(y_true) / len(y_true)
+    ssr = sum((y_pred - y_true) ** 2)
+    sst = sum((y_true - y_mean) ** 2)
+    r2 = 1 - (ssr / sst)
+    return r2
 
 
 def mse(y_pred, y_true):
@@ -64,10 +83,8 @@ def mse(y_pred, y_true):
     mse - mean squared error
     """
 
-    """
-    YOUR CODE IS HERE
-    """
-    pass
+    mse = sum((y_pred - y_true) ** 2) / len(y_true)
+    return mse
 
 
 def mae(y_pred, y_true):
@@ -80,8 +97,6 @@ def mae(y_pred, y_true):
     mae - mean absolut error
     """
 
-    """
-    YOUR CODE IS HERE
-    """
-    pass
+    mae = sum(abs(y_pred - y_true)) / len(y_true)
+    return mae
     
